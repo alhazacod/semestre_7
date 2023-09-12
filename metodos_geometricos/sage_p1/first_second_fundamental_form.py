b0e5384eacaed0598321a1948ca0b634652a7b53
alhazacod <manuelangelgarcia@protonmail.com>
2023-09-12 16:04:25 -0500

    Clase de cantica 1 12-sep

--- metodos_geometricos/sage_p1/first_second_fundamental_form.py
+++ metodos_geometricos/sage_p1/first_second_fundamental_form.py
@@ -1,13 +1,50 @@
 from sage.all import *
 
+def print_dict(dictionary):
+    for key, value in dictionary.items():
+        print(f'{key}: {value}')
+
+def calc_first_derivatives(surface ):
+    first_derivative_ = surface.natural_frame()
+    first_derivative = {'u': first_derivative_[1], 'v': first_derivative_[1]}
+    return first_derivative
+    
+
+def calc_second_derivatives(surface ):
+    first_derivative = calc_first_derivatives(surface)
+    second_derivative = {'uu': derivative(first_derivative['u'],u),
+                         'uv': derivative(first_derivative['u'],v),
+                         'vv': derivative(first_derivative['v'],v),
+                         'vu': derivative(first_derivative['v'],u),}
+    return second_derivative
+
+def dot_products(surface ):
+    first_derivative = calc_first_derivatives(surface)
+    second_derivative = calc_second_derivative(surface)
+    normal = surface.normal_vector()
+
 u,v,a,b = var('u,v,a,b',domain='real')
 
 eq = [a*cos(u)*cos(v), a*sin(u)*cos(v), a*sin(v)]
-
 surface = ParametrizedSurface3D(eq, [u,v])
 
-first_derivative = surface.natural_frame()
-print(first_derivative)
+print(f'{surface}\n')
 
-second_derivative_u = derivative(first_derivative[1],u)
-print(second_derivative_u)
+print('First derivatives: ')
+print_dict(calc_first_derivatives(surface))
+print('\n')
+
+print('Second derivatives:')
+print_dict(calc_second_derivatives(surface))
+print('\n')
+
+print(f'Vector normal: {surface.normal_vector()} ')
+print('\n')
+
+print('First fundamental form:')
+print_dict(surface.first_fundamental_form_coefficients())
+print('\n')
+
+print('Second fundamental form:')
+print_dict(surface.second_fundamental_form_coefficients())
+print('\n')
